25b410e87cb8e7e06b5687ed3b2203a9774cec6d
sarthak ganure <sarthakganure@sarthaks-MacBook-Air.local>
2023-10-15 15:49:57 +0530

    created fancy/ and added two scripts in it

---
+++ fancy/night_sky.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Define colors
+blue='\e[1;34m'
+yellow='\e[1;93m'
+reset='\e[0m'
+
+# Function to draw stars
+draw_stars() {
+    for i in {1..30}; do
+        x=$((RANDOM % $(tput lines)))
+        y=$((RANDOM % $(tput cols)))
+        tput cup $x $y
+        echo -en "${yellow}*"
+    done
+}
+
+# Function to display starry sky
+display_starry_sky() {
+    clear
+    draw_stars
+    sleep 1
+}
+
+# Main function
+main() {
+    for i in {1..10}; do
+        display_starry_sky
+    done
+
+    tput cup $(tput lines) 0
+}
+
+# Call the main function
+main
