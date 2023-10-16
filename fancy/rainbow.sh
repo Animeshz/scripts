30e754ec0725f45d9c93da00c2391855ed529b79
Sarthak Ganure <116540752+SaRTHaK0909k@users.noreply.github.com>
2023-10-16 12:44:54 +0530

    Adding Scripts in #1 (#8)
    * created fancy/ and added two scripts in it
    
    * added chmod +x fancy/*
    
    ---------
    
    Co-authored-by: sarthak ganure <sarthakganure@sarthaks-MacBook-Air.local>

---
+++ fancy/rainbow.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Define colors
+red='\e[1;31m'
+orange='\e[1;33m'
+yellow='\e[1;93m'
+green='\e[1;32m'
+blue='\e[1;34m'
+indigo='\e[1;94m'
+violet='\e[1;35m'
+reset='\e[0m'
+
+# Rainbow colors array
+colors=($red $orange $yellow $green $blue $indigo $violet)
+
+# Function to draw rainbow line
+draw_rainbow_line() {
+    for color in "${colors[@]}"; do
+        echo -en "$color#####"
+        sleep 0.1
+    done
+    echo -e "$reset"
+}
+
+# Function to draw the rainbow
+draw_rainbow() {
+    clear
+    for i in {1..7}; do
+        draw_rainbow_line
+        sleep 0.1
+    done
+}
+
+# Main function
+main() {
+    draw_rainbow
+    echo "Rainbow has been drawn!"
+}
+
+# Calling the main function
+main
